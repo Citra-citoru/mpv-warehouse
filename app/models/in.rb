a3fbbfe77a1b517af977aae296dcd98d63ff5db5
citra-citoru <citra.rahman@happy5.co>
2023-09-21 10:34:05 +0300

    change models logic

--- app/models/in.rb
+++ app/models/in.rb
@@ -1,3 +1,26 @@
 class In < ApplicationRecord
   belongs_to :stock
+
+  validates :date, :document, :number, :quantity, :unit_price, :total_price, :stock, presence: true
+  validates :quantity, :unit_price, :total_price, numericality: true
+
+  after_create :after_create_in
+  after_destroy :after_destroy_in
+
+  private
+
+  def after_create_in
+    total_price = self.quantity = self.unit_price
+    self.update(total_price: total_price)
+    average_cost = ((self.stock.stock_balance * self.unit_price) + (self.quantity * self.unit_price))
+    stock_balance *= average_cost
+    total_cost *= average_cost
+    self.stock.update(average_cost: average_cost, stock_balance: stock_balance, total_cost: total_cost)
+  end
+
+  def after_destroy_in
+    stock_balance = self.stock.stock_balance - self.quantity
+    total_cost = stock_balance * self.stock.average_cost
+    self.stock.update(stock_balance: stock_balance, total_cost: total_cost)
+  end
 end
--- app/models/requisition.rb
+++ app/models/requisition.rb
@@ -1,3 +1,42 @@
 class Requisition < ApplicationRecord
   belongs_to :stock
+  # Validations
+  validates :kind, :date, :number, :department, :stock, presence: true
+  validates :quantity, :average_cost, :total_cost, numericality: true
+
+  # Enumerators
+  enum kind: %i[Requisition Returns]
+  enum department: %i[Kitchen Bar Restaurant]
+
+  # Callback
+  after_create :after_create_requisition
+  after_destroy :after_destroy_requisition
+
+  # Methods Privates
+  private
+
+  def after_create_requisition
+    if kind == 'Requisition'
+      update(average_cost: stock.average_cost, total_cost: (quantity * stock.average_cost))
+      stock_balance = stock.stock_balance - quantity
+      total_cost = stock_balance * average_cost
+      stock.update(stock_balance: stock_balance, total_cost: total_cost)
+    elsif update(average_cost: stock.average_cost, total_cost: (quantity * stock.average_cost))
+      stock_balance = stock.stock_balance + quantity
+      total_cost = stock_balance * average_cost
+      stock.update(stock_balance: stock_balance, total_cost: total_cost)
+    end
+  end
+
+  def after_destroy_requisition
+    if kind == 'Requisition'
+      stock_balance = stock.stock_balance + quantity
+      total_cost = stock_balance * average_cost
+      stock.update(stock_balance: stock_balance, total_cost: total_cost)
+    else
+      stock_balance = stock.stock_balance - quantity
+      total_cost = stock_balance * average_cost
+      stock.update(stock_balance: stock_balance, total_cost: total_cost)
+    end
+  end
 end
